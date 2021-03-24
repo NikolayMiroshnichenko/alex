0eb4c98a441fec25aa9e558ebdde0f3bab3b9b24
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2021-03-24 20:28:52 +0200

    first commit

---
+++ js/header.js
@@ -0,0 +1,7 @@
+// let body = document.querySelector("body");
+
+// window.addEventListener("scroll", () => {
+//     const posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+//     console.log(posTop)
+// });
---
+++ js/slider.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".swiper-container", {
+  spaceBetween: 40,
+  // centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    990: {
+      slidesPerView: 3,
+    },
+    1220: {
+      slidesPerView: 4,
+    },
+  },
+});
