980c4d44598c176e6bd5485ba0e9101e5eb19613
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2021-03-31 12:37:22 +0300

    fix-slider-click

--- js/anchors.js
+++ js/anchors.js
@@ -1,14 +1,6 @@
-const anchors = document.querySelectorAll(".anchors");
-
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    const blockID = anchor.getAttribute("href").substr(1);
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+$(document).ready(function(){
+  $( "a.anchors" ).click(function( event ) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
   });
-}
+});
